255039e90c99b757073edccd743ab525a430f192
DeXoteric <marlev@poczta.pl>
2018-05-12 15:34:35 +0200

    ball gfx, ball physics

---
+++ Assets/Scripts/DefaultBallController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class DefaultBallController : MonoBehaviour
+{
+    [SerializeField] private float startDrag = 0.4f;
+    [SerializeField] private float stopDrag = 1f;
+
+    private const float STOP_SPEED = 0.1f;
+
+    private Rigidbody2D energyBall;
+
+    private void Start()
+    {
+        energyBall = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (energyBall.velocity.magnitude <= STOP_SPEED)
+        {
+            energyBall.drag = stopDrag;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        energyBall.drag = startDrag;
+    }
+}
---
+++ Assets/Scripts/WhiteBallController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class WhiteBallController : MonoBehaviour
+{
+    [Range(10f, 100f)] [SerializeField] private float forceMultiplier = 100f;
+    [SerializeField] float startDrag = 0.4f;
+    [SerializeField] float stopDrag = 1f;
+
+    private const float STOP_SPEED = 0.1f;
+
+    private Rigidbody2D energyBall;
+    private Vector2 currentMousePosition, currentWhiteBallPosition;
+
+    private void Start()
+    {
+        energyBall = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        print(energyBall.velocity.magnitude);
+        if (energyBall.velocity.magnitude <= STOP_SPEED)
+        {
+            energyBall.drag = stopDrag;
+        }
+    }
+
+    private void OnMouseDrag()
+    {
+        
+        Vector3 currentScreenMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        currentMousePosition = new Vector2(currentScreenMousePosition.x, currentScreenMousePosition.y);
+    }
+
+    private void OnMouseUp()
+    {
+        energyBall.drag = startDrag;
+        currentWhiteBallPosition = new Vector2(energyBall.position.x, energyBall.position.y);
+
+        float directionX = currentWhiteBallPosition.x - currentMousePosition.x;
+        float directionY = currentWhiteBallPosition.y - currentMousePosition.y;
+
+        Vector2 direction = new Vector2(directionX, directionY);
+
+        direction.Normalize();
+
+        energyBall.AddForce(direction * forceMultiplier, ForceMode2D.Impulse);
+    }
+}
